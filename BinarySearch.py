c29eb9eadb7d6a9b6bd33812584bd1d4c5f14485
ramana <gamerdude9@gmail.com>
2017-07-08 17:12:29 +0530

    improved binary search from recussion to while loop

--- BinarySearch.py
+++ BinarySearch.py
@@ -15,6 +15,30 @@ def find(key):
 	i = findInRange(key,numberslist,0,len(numberslist),0)
 	print "found it at position :"+ str(i[0]) +"with recursion of :" + str(i[1])
 
-find(999)
+# find(999)
+
+
+# improved binary search with while loop and less variables and used it in prime numbers
+
+primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71]
+
+def is_prime(num,count):
+	i = 0
+	j = len(primes)
+	while (j>=i):
+		count = count + 1 
+		guess = (i+j)/2	
+		if primes[guess] == num:
+			return guess,count
+		elif primes[guess] > num:
+			j = guess -1
+		else:i = guess +1
+	return None,count
+
+prime_pos = is_prime(22,0)
+
+print "found "+ str(prime_pos[0]) +" with recursion of :" + str(prime_pos[1])
+
+
 
 
--- routeFinder.py
+++ routeFinder.py
@@ -1,1 +1,47 @@
-# need to implement a simple path finding game in python. 
+# need to implement a simple path finding game in python.
+# import pprint 
+board = [[0]*7 for _ in range(7)]
+
+def print_grid(grid):
+    for row in grid:
+        for e in row:
+            print e,
+        print  
+
+# print print_grid(board)
+
+def set_goal(a,b):
+	board[a][b] = "g"	
+
+# set_goal(3,3)
+# print print_grid(board)
+
+def set_block(a,b):
+	board[a][b] = "b"
+
+set_block(4,2)
+set_block(2,2)
+set_block(3,2)
+set_block(5,2)
+set_block(1,0)
+set_block(1,2)
+set_block(1,1)
+
+print print_grid(board)
+
+def fill(board,a,b):
+	if a<5 and b<5 and a>1 and b>1: 
+		if not board[a-1][b] == "b" and board[a-1][b] == 0: board[a-1][b] = board[a][b] + 1
+		if not board[a][b+1] == "b" and board[a][b+1] == 0: board[a][b+1] = board[a][b] + 1
+		if not board[a+1][b] == "b" and board[a+1][b] == 0: board[a][b+1] = board[a][b] + 1
+		if not board[a][b-1] == "b" and board[a][b-1] == 0: board[a][b+1] = board[a][b] + 1
+		fill(board,a-1,b)
+		fill(board,a,b+1)
+		fill(board,a+1,b)
+		fill(board,a,b+1)
+	return 
+
+fill(board,3,3)
+
+print_grid(board)
+
